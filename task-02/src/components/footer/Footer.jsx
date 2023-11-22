76c0bad83e222586efca85ae076aac3715c7ebbe
T.Phaneendra Nikhil <tallapragadaphaneendranikhil@gmail.com>
2023-11-22 13:42:36 +0530

    Add files via Upload

---
+++ task-02/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+
+const Footer = () => {
+
+  
+  return (
+    <>
+      <div className="footer-container" >
+        <p className="copy-right">
+          &copy; phaneendra nikhil 2023. Inspired from <span>DevKnus</span>. All rights reserved.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
